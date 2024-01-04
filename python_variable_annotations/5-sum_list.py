c6cf1c23518b91b815234c506564fe21d72be39c
sarahmarkland <sarah.markland@icloud.com>
2024-01-03 19:27:59 -0600

    task 5 v2

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - list of floats """
+import typing
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """ Take a float and return string """
     return sum(input_list)
