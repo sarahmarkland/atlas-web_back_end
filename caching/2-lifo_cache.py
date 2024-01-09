bd9c9d4d65d97e37d6af70f7cec8209d01cf39d1
sarahmarkland <sarah.markland@icloud.com>
2024-01-09 15:29:20 -0600

    task 2?

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+''' LIFO caching system '''
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    ''' LIFO caching system '''
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        ''' add item to cache '''
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discard = self.queue.pop()
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        ''' get item from cache '''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
