efd0d0ba2df611a64776aa423a1f64fef12af64c
sarahmarkland <sarah.markland@icloud.com>
2024-01-09 14:37:22 -0600

    task 0 documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,11 +9,13 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        ''' add item to cache '''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        ''' get item from cache '''
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
