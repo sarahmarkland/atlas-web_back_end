decb14cafed2aea72978abb87421b72d36ff52bc
sarahmarkland <sarah.markland@icloud.com>
2024-01-10 13:35:22 -0600

    task 1, remove task 2 stuff done in wrong file

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -43,16 +43,3 @@ class Server:
             return []
         idx_range = index_range(page, page_size)
         return self.__dataset[idx_range[0]:idx_range[1]]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """return a dictionary containing the following key-value pairs"""
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.__dataset) / page_size)
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
-        }
