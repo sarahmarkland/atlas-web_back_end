51399c5c63ebf1b1f2108efcc5c78e068bf48633
sarahmarkland <sarah.markland@icloud.com>
2024-01-15 17:53:21 -0600

    task 0 remove Nones and add data types

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,17 +4,8 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    if not fields or not isinstance(fields, list):
-        return
-    if not separator or not isinstance(separator, str):
-        return
-    if not message or not isinstance(message, str):
-        return
-    if not redaction or not isinstance(redaction, str):
-        return
-
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
