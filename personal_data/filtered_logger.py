3d138f01d7ab83a73093f08c4861e35048115d55
sarahmarkland <sarah.markland@icloud.com>
2024-01-15 17:38:51 -0600

    task 0 exceptions

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,15 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
+    if not fields or not isinstance(fields, list):
+        return
+    if not separator or not isinstance(separator, str):
+        return
+    if not message or not isinstance(message, str):
+        return
+    if not redaction or not isinstance(redaction, str):
+        return
+
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
