4c192a608b1dea0b13a7afc4d884464434867375
sarahmarkland <sarah.markland@icloud.com>
2024-01-16 13:28:39 -0600

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,6 +14,7 @@ def filter_datum(fields: typing.List[str], redaction: str, message: str,
                          field + "=" + redaction + separator, message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
 
@@ -21,7 +22,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields:typing.List[str] = None):
+    def __init__(self, fields: typing.List[str] = None):
         """
         Initialize the RedactingFormatter.
 
