2aa17ea4a1b3e3ec970052e791e9a50083a08e72
sarahmarkland <sarah.markland@icloud.com>
2024-01-15 18:06:43 -0600

    task 0 typing.List annotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """Regex-ing"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
