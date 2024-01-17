9e76c0daa616d3290514de1c90baf8790e243edc
sarahmarkland <sarah.markland@icloud.com>
2024-01-17 14:17:32 -0600

    task 8 or something

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """Basic authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract base64 authorization header"""
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
