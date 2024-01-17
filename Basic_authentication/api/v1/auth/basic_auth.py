7e32c347f31c76a48161884ba41878af7589048f
sarahmarkland <sarah.markland@icloud.com>
2024-01-17 14:14:08 -0600

    task 6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Basic authentication"""
-from api.v1.auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-"""Basic auth class"""
-    def __init__(self):
-    """Constructor"""
-        pass
+    """BasicAuth class"""
+    pass
