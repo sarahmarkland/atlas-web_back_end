83d1a5d922bc8c2f5017f182937a0700df5aacc7
sarahmarkland <sarah.markland@icloud.com>
2024-01-17 14:19:06 -0600

    task 7 pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,9 +6,11 @@ import base64
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """Extract base64 authorization header"""
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
             return None
         if authorization_header[:6] != 'Basic ':
             return None
