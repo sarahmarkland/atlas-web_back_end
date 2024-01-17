8d57d39ee49f00371cec40aacd543f31aba29e7c
sarahmarkland <sarah.markland@icloud.com>
2024-01-17 13:58:24 -0600

    task 5 updat auth.py app.py

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic authentication"""
+
+
+class BasicAuth(Auth):
+    """Basic auth class"""
+        def __init__(self):
+            """Constructor"""
+            pass
