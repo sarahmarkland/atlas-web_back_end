4bfde72f9638392070b933c607d882a92aab29db
sarahmarkland <sarah.markland@icloud.com>
2024-01-17 15:13:07 -0600

    task 10 pycode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,19 +37,19 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':', 1))
-    
-    def user_object_from_credentials(self, user_email: str, user_pwd: str
-                                        ) -> TypeVar('User'):
-            """User object from credentials"""
-            if user_email is None or not isinstance(user_email, str):
-                return None
-            if user_pwd is None or not isinstance(user_pwd, str):
-                return None
-            try:
-                users = User.search({'email': user_email})
-            except Exception:
-                return None
-            for user in users:
-                if user.is_valid_password(user_pwd):
-                    return user
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """User object from credentials"""
+        if user_email is None or not isinstance(user_email, str):
             return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
