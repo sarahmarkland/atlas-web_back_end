954a5c176831fd96f51ed328e99570c6a9bc75a4
sarahmarkland <sarah.markland@icloud.com>
2024-01-23 19:36:14 +0000

    task 0 v3 or 4

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -29,9 +29,12 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id is 'me' and request.current_user is None:
         abort(404)
-    user = User.get(user_id)
     if user_id is 'me' and request.current_user:
-        return jsonify(user.to_json())
+        return jsonify(request.current_user.to_json())
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
