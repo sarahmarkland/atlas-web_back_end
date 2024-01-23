df9752504072a2897a12999c4d23b8d32b71267d
sarahmarkland <sarah.markland@icloud.com>
2024-01-23 21:32:14 +0000

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,4 +5,20 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """SessionAuth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user id for session id"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
