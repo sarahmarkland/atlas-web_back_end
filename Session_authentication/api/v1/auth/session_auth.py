40c49e3b9133ef48c0fa9ce8a64d7754df3790fe
sarahmarkland <sarah.markland@icloud.com>
2024-01-23 21:44:40 +0000

    task 2 v3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
 
+
 class SessionAuth(Auth):
     """SessionAuth class"""
     user_id_by_session_id = {}
@@ -15,7 +16,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """user id for session id"""
         if session_id is None or type(session_id) != str:
