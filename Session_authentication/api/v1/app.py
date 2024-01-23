f9208371c2a70b1a8a37584d6e9e4fcdf756f204
sarahmarkland <sarah.markland@icloud.com>
2024-01-23 21:13:54 +0000

    idk

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -3,16 +3,27 @@
 Route module for the API
 """
 from os import getenv
+import os
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+#determine which authentication method to use
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
 
 if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" SessionAuth inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    pass
