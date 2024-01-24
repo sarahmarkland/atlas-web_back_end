30644fbd7e24dd2d35e601b64108c4c98ab9b56d
sarahmarkland <sarah.markland@icloud.com>
2024-01-24 18:10:16 +0000

    eat karis' nana

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -22,7 +22,7 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """current user"""
         session_id = self.session_cookie(request)
