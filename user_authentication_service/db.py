1a84a78fbddf7853b4c1a945e520f309fa73b6ff
sarahmarkland <sarah.markland@icloud.com>
2024-01-25 01:31:20 +0000

    wow indent

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """DB module"""
 from sqlalchemy import create_engine
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -12,10 +11,9 @@ from user import User
 
 class DB:
     """DB class"""
-
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
