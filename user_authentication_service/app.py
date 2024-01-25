e831f2a3ff14ad6b089ff68ea7a8cf7935bf001b
sarahmarkland <sarah.markland@icloud.com>
2024-01-25 03:04:40 +0000

    um

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+basic Flask app
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+from flask.helpers import make_response
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ GET /
+    Return:
+      - welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
