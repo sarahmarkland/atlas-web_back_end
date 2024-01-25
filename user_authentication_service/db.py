c59399291059a9e458b3e1fabefdcc761fb6bdd1
sarahmarkland <sarah.markland@icloud.com>
2024-01-25 01:43:17 +0000

    task 3?

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
@@ -32,3 +33,12 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Method that returns the first row found in the users table"""
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
