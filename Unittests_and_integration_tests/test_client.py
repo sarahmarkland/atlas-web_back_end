4ba9ecc3a23f4008d38335d3a762ae06a974114c
sarahmarkland <sarah.markland@icloud.com>
2024-01-30 01:16:17 +0000

    task 5 not passing tests

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -18,5 +18,17 @@ class TestGithubOrgClient(unittest.TestCase):
         org_result = github_client.org()
         mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
 
+    def test_public_repos_url(self):
+        """ method to return known payload """
+        known_payload = {"repos_url": "https://api.github.com/orgs/example/repos"}
+
+        with patch('client.GithubOrgClient.org', return_value=known_payload):
+            github_client = GithubOrgClient("example")
+
+            result = github_client._public_repos_url
+
+            expected_result = "https://api.github.com/orgs/example/repos"
+            self.assertEqual(result, expected_result)
+
 if __name__ == '__main__':
     unittest.main()
