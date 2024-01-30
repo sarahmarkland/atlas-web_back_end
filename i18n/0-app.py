63716755da7db14f6d42e5b0138661bc5a5338b2
sarahmarkland <sarah.markland@icloud.com>
2024-01-30 19:20:38 +0000

    task 0 v4

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+""" Basic Flask app """
 from flask import Flask, render_template
 # from flask_babel import Babel
 
 app = Flask(__name__)
 # babel = Babel(app)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """ GET / """
+    """ create a single route and an index.html """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
