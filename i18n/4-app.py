87df2fb8266b675665d58d80c4954196a6274209
sarahmarkland <sarah.markland@icloud.com>
2024-01-30 20:15:12 +0000

    task 4

---
+++ i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" force locale with URL parameter """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Babel config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ create a single route and an index.html """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def gettext():
+    """ gettext function """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
