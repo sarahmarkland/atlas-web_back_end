5955fa839f95562dd2fdb2191ad0c87dc6738d1e
sarahmarkland <sarah.markland@icloud.com>
2024-01-30 19:17:45 +0000

    task 0v3 lol

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
-from flask_babel import Babel
+# from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
+# babel = Babel(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return render_template('index.html', title='Welcome to Holberton',\
-                           header='Hello world')
+    """ GET / """
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
