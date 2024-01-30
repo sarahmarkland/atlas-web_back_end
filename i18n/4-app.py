206f05777a7a221397dee6dab668b823c17f1406
sarahmarkland <sarah.markland@icloud.com>
2024-01-30 21:07:21 +0000

    task 3

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -32,6 +32,8 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+babel.init_app(app, locale_selector=get_locale)
+
 
 def gettext():
     """ gettext function """
