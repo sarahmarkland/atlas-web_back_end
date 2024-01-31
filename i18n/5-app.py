e1e09cfb26ec2f41ed35e05ec1d22e8452bd8050
sarahmarkland <sarah.markland@icloud.com>
2024-01-31 19:04:41 +0000

    task 5 app.py updates

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ force locale with URL parameter """
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -18,10 +18,32 @@ class Config(object):
 app.config.from_object(Config)
 
 
+@app.before_request
+def before_request():
+    """ before request """
+    g.user = get_user()
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """ get user """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ create a single route and an index.html """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 @babel.localeselector
@@ -30,14 +52,6 @@ def get_locale():
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
-
-
-def gettext():
-    """ gettext function """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
