9da4e227ddab9894d59ee4ff9663b13616729525
sarahmarkland <sarah.markland@icloud.com>
2024-01-31 20:03:07 +0000

    love a merge conflict for me

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -4,16 +4,20 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
+
+def get_user():
+    """ method to get user """
+    userID = request.args.get('login_as')
+    if userID:
+        return users.get(int(userID))
+    return None
 
 
 class Config:
@@ -23,24 +27,9 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+babel = Babel(app)
 app.config.from_object(Config)
-
-
-def get_user():
-    """ method to get user """
-    try:
-        return users.get(int(request.args.get('login_as')))
-    except Exception:
-        return None
-    
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """ GET /
-    Return:
-      - 6-index.html
-    """
-    return render_template('6-index.html')
 
 
 @babel.localeselector
@@ -58,5 +47,11 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.route('/')
+def index():
+    """ index route """
+    return render_template('6-index.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
