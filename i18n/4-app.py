95fab1b3bcf2623846130d3bbba6493b52af1f7e
sarahmarkland <sarah.markland@icloud.com>
2024-01-31 18:41:31 +0000

    remove underscore from import

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ force locale with URL parameter """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 app = Flask(__name__)
