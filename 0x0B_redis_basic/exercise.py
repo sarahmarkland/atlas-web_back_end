6ec03706f3114f7682a33a63b4cbefe89543e63c
sarahmarkland <sarah.markland@icloud.com>
2024-02-06 15:06:54 -0600

    task 0 -.-

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,16 @@
+""" writing strings to redis """
+import redis
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """ Constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        """ Store data in redis """
+        key = self._redis.get('key')
+        self._redis.set('key', data)
+        return key
