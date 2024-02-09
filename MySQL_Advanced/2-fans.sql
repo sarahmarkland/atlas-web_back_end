3a98538a82e2505d461fb0af36e4a9ef14249e57
sarahmarkland <sarah.markland@icloud.com>
2024-02-09 14:55:15 -0600

    task 2 sqfml

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -2,14 +2,6 @@
 -- ordered by the nuymber of (non-unique) fans in each country
 
 SELECT origin, SUM(nb_fans) AS nb_fans
-FROM (
-    SELECT
-        CASE
-            WHEN origin LIKE '%,%' THEN origin
-            ELSE origin
-        END AS origin,
-        nb_fans
-    FROM metal_bands
-) AS band_countries
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
