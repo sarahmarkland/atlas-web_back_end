6302a53bcdde0adf9e8b18d17d4f22a0b84f5696
sarahmarkland <sarah.markland@icloud.com>
2024-02-09 14:57:37 -0600

    task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- write an SQL script that ranks country origins of bands
 -- ordered by the nuymber of (non-unique) fans in each country
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
