0946ea97d8477b6b211cc2375e20c8a444d51911
sarahmarkland <sarah.markland@icloud.com>
2024-02-11 17:23:55 -0600

    task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,11 +1,20 @@
 -- Write a SQL script that creates a trigger that decreases the quantity
 -- of an item after adding a new order
+DELIMITER //
 
-CREATE TRIGGER decrease_quantity
+CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+    DECLARE item_id INT;
+    DECLARE order_quantity INT;
+
+    SELECT NEW.item_id, NEW.quantity INTO item_id, order_quantity;
+
     UPDATE items
-    SET quantity = quantity - 1
-    WHERE id = NEW.item_id;
+    SET quantity = quantity - order_quantity
+    WHERE id = item_id;
 END;
+//
+
+DELIMITER ;
