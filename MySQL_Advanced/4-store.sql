07886a3eb64dfc42b2034ea91205be252b403db8
sarahmarkland <sarah.markland@icloud.com>
2024-02-11 17:50:12 -0600

    task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -7,8 +7,8 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.amount
-    WHERE name = NEW.item_id;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
 END;
 //
 
