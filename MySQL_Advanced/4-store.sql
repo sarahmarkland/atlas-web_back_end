befe535ca258768e41eed0e394d74e6893639b36
sarahmarkland <sarah.markland@icloud.com>
2024-02-11 17:31:57 -0600

    task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -6,11 +6,6 @@ CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    DECLARE item_id INT;
-    DECLARE order_quantity INT;
-
-    SELECT NEW.item_id, NEW.quantity INTO item_id, order_quantity;
-
     UPDATE items
     SET quantity = quantity - NEW.amount
     WHERE id = item_id;
