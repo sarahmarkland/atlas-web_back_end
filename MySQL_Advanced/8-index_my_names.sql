ee6f3ca4b35ab72c744c9f79b93805cefb9bcafe
sarahmarkland <sarah.markland@icloud.com>
2024-02-11 19:58:25 -0600

    task 8

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,9 +1,3 @@
 -- create an index on the name column of the users table
 
-USE holberton;
-
--- drop the index if it exists
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- create the index on first letter of name
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names (name(1));
