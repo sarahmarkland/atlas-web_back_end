0ba9d8618297dabf1ed41a16a474458e04423ca4
sarahmarkland <sarah.markland@icloud.com>
2024-02-11 19:01:10 -0600

    task 6

---
+++ MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,26 @@
+-- SQL script that creates a stored procedure named AddBonus that adds
+-- a new correction for a student
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+    IN p_user_id INT,
+    IN p_project_name VARCHAR(255),
+    IN p_bonus INT
+)
+BEGIN
+    DECLARE project_id INT;
+
+-- Check if the project exists, if not, create it
+    SELECT id INTO project_id FROM projects WHERE name = p_project_name;
+
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (p_project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+-- Add the correction
+INSERT INTO corrections (user_id, project_id, score) VALUES (p_user_id, project_id, p_bonus);
+END;
+//
+
+DELIMITER ;
