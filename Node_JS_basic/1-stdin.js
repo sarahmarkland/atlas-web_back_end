d3d7e8c3929d170e8fbad41ec387ecb46bee53c1
sarahmarkland <sarah.markland@icloud.com>
2024-02-21 16:56:02 -0600

    task 1 revert to previous version

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,11 +8,10 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  process.exit();
 }
 );
 
-process.stdin.on('end', () => {
+process.stdin.on('exit', () => {
   console.log('This important software is now closing');
 }
 );
