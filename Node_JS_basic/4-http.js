97d241a7be6a667022f51a05f6b9db3600a05125
sarahmarkland <sarah.markland@icloud.com>
2024-02-25 13:14:39 -0600

    task 4 eslint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -6,8 +6,7 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   res.end('Hello Holberton School!');
-}
-);
+});
 
 const PORT = 1245;
 app.listen(PORT);
