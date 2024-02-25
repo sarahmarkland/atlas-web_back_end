4bd2eeb784c1ae92b07076b18578b487c598479e
sarahmarkland <sarah.markland@icloud.com>
2024-02-25 13:10:42 -0600

    task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+// create a small HTTP server using Node.js http module
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('Hello Holberton School!\n');
+}
+);
+
+const PORT = 1245;
+app.listen(PORT);
+
+module.exports = app;
