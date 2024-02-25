4e1995738480ff5b8ae3a7b5eaaf5a9abefd0dc5
sarahmarkland <sarah.markland@icloud.com>
2024-02-25 12:03:39 -0600

    task2 v3

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -13,15 +13,18 @@ function countStudents(path) {
     const lines = data.trim().split('\n').slice(1);
     const students = {};
 
-    for (const line of lines) {
-      const student = line.split(',');
-      if (!students[student[3]]) {
-        students[student[3]] = [];
-      }
-      students[student[3]].push(student[0]);
+    students.forEach((student) => {
+      const [firstName,, , field] = student.split(',');
+      if (!students[field]) students[field] = [];
+      students[field].push(firstName);
     }
+    );
     console.log(`Number of students: ${lines.length}`);
-    for (const [cls, names] of Object.entries(students)) {
-      console.log(`Number of students in ${cls}: ${names.length}. List: ${names.join(', ')}`);
+
+    Object.entries(students).forEach(([field, names]) => {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
-  }
+    );
+}
+
+module.exports = countStudents;
