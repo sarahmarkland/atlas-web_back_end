a03dc920b71a63448d1734d7bda64a8218a8cf34
sarahmarkland <sarah.markland@icloud.com>
2024-02-26 14:19:18 -0600

    task 7 eslint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -5,26 +5,26 @@ const app = express();
 
 // Route for the root path
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 // Route for /students path
 app.get('/students', (req, res) => {
-    const databasePath = process.argv[2];
-    countStudents(databasePath)
-        .then((students) => {
-            res.send(`This is the list of our students: \n${JSON.stringify(students)}`);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Internal Server Error');
-        });
+  const databasePath = process.argv[2];
+  countStudents(databasePath)
+    .then((students) => {
+      res.send(`This is the list of our students: \n${JSON.stringify(students)}`);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 // Listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
 // Export the app variable
