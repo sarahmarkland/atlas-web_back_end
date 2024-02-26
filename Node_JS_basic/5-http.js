52b9f6c74c01d46fc4841956c0241230bed77baa
sarahmarkland <sarah.markland@icloud.com>
2024-02-25 18:24:50 -0600

    task 5 oh boy

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+// const fs = require('fs');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const databasePath = process.argv[2];
+    countStudents(databasePath)
+      .then(() => {
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        console.error(error);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
