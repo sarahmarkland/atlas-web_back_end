8f5d3c310d0a239a00199d9c381a7267cfb71827
sarahmarkland <sarah.markland@icloud.com>
2024-02-25 18:46:28 -0600

    task 6

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
-// const fs = require('fs');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+// const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
